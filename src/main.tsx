7c88dd7a03085a4e9db27bcfd6a04de67d2a8c93
Mnigos <makowskiigor@gmail.com>
2024-09-03 18:19:06 +0200

    feat: add users table

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-// import App from './app.tsx'
+import App from './app.tsx'
 
-// import './globals.css'
+import './globals.css'
 
-// ReactDOM.createRoot(document.querySelector('#root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
+ReactDOM.createRoot(document.querySelector('#root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
