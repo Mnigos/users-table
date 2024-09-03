cb4aaf8261a9cd80899f4085bcbd47f995f79e3c
Mnigos <makowskiigor@gmail.com>
2024-09-03 18:09:12 +0200

    feat(env): setup variables

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
 import { createEnv } from '@t3-oss/env-core'
-// import { z } from 'zod'
+import { z } from 'zod'
 
 export const env = createEnv({
   clientPrefix: 'VITE_',
-  client: {},
-  runtimeEnv: process.env,
+  client: {
+    VITE_JSON_PLACEHOLDER_API_URL: z.string().url(),
+  },
+  runtimeEnv: import.meta.env,
   emptyStringAsUndefined: true,
 })
